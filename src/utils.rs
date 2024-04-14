fb757fc14148eab22dcf11722163f284cf12dea5
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-04-14 13:58:16 +1000

    feat/refactor: cleanup error handling code

--- src/utils.rs
+++ src/utils.rs
@@ -1,31 +1,25 @@
-use anyhow::Result;
-use tasklist::Process;
+// use tasklist::Process;
+use crate::prelude::Process;
 
 pub struct Utils {}
 
 impl Utils {
-    pub fn test_if_match(process: &Process, target_variations: &[String]) -> Result<Vec<Process>> {
+    pub fn test_if_match(process: &Process, target_variations: &[String]) -> Vec<Process> {
         let mut matched_processes: Vec<Process> = Vec::new();
 
-        match target_variations
+        if target_variations
             .iter()
-            .find(|&variation| process.pname.to_lowercase().contains(variation))
+            .any(|variation| process.pname.to_lowercase().contains(variation))
         {
-            Some(_) => {
-                matched_processes.push(Process {
-                    pid: process.pid,
-                    pname: process.pname.clone(),
-                });
-            }
-            None => {
-                // Current wasn't found in the target variations, do nothing.
-            }
+            matched_processes.push(Process {
+                pid: process.pid,
+                pname: process.pname.clone(),
+            });
         }
-
-        Ok(matched_processes)
+        matched_processes
     }
 
-    pub fn generate_variations(app_name: String) -> Vec<String> {
+    pub fn generate_variations(app_name: &str) -> Vec<String> {
         vec![
             app_name.to_lowercase(),
             format!("{}{}", app_name.to_lowercase(), ".exe"),
